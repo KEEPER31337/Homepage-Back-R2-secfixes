ad141860cf4f9d9eacd55c1d12cf696c02b8aafe
HSHwan <tnghks2866@gmail.com>
2026-03-24 16:26:19 +0900

    fix: 제목+내용 검색에서 비밀글 제외

--- src/main/java/com/keeper/homepage/domain/post/dao/PostRepository.java
+++ src/main/java/com/keeper/homepage/domain/post/dao/PostRepository.java
@@ -95,6 +95,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
       + "WHERE p.category = :category "
       + "AND p.postStatus.isNotice = false "
       + "AND p.postStatus.isTemp = false "
+      + "AND p.postStatus.isSecret = false "
       + "AND (LOWER(p.postContent.title) LIKE LOWER(concat('%', :search, '%')) "
       + "OR LOWER(p.postContent.content) LIKE LOWER(concat('%', :search, '%'))) "
       + "ORDER BY p.registerTime DESC")
